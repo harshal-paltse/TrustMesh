d3ca7539e310816de8042edd1affecc32861ed85
Harshal Paltse <148197108+harshal-paltse@users.noreply.github.com>
2026-08-23 22:13:49 +0530

    Update build.gradle.kts

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     // Argon2 Password Hashing
     implementation("de.mkammerer:argon2-jvm:2.11")
 
-    // Logging
+    // Log
     implementation("ch.qos.logback:logback-classic:1.4.14")
 
     // Testing
