3726a4579e0cdd97314c61547c73c8f7fabadbc7
Harshal Paltse <148197108+harshal-paltse@users.noreply.github.com>
2026-08-31 23:23:22 +0530

    Update build.gradle.kts

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
 
     implementation("ch.qos.logback:logback-classic:1.4.14")
 
+
     // Testing
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.0.21")
