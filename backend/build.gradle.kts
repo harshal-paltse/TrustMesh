561dff427a5ae5186173e3ffbfd452d0b11168d6
Harshal Paltse <148197108+harshal-paltse@users.noreply.github.com>
2026-09-14 23:25:48 +0530

    Remove empty line in build.gradle.kts

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -53,7 +53,6 @@ dependencies {
 
     implementation("ch.qos.logback:logback-classic:1.4.14")
 
-
     // Testing
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.0.21")
