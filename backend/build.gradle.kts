d4dfee695869293fccee99464ebaf3050c10f86c
Harshal Paltse <148197108+harshal-paltse@users.noreply.github.com>
2026-09-14 23:24:39 +0530

    Update Kotlin version and add JVM target

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
     id("application")
 }
 
-
 group = "com.trustmesh"
 version = "1.0.0"
 
 application {
     mainClass.set("com.trustmesh.ApplicationKt")
 }
+
 
 repositories {
     mavenCentral()
@@ -53,10 +53,12 @@ dependencies {
 
     implementation("ch.qos.logback:logback-classic:1.4.14")
 
+
     // Testing
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.0.21")
 }
+
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
